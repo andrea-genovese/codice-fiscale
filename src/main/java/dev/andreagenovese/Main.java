97c13f560a860074d521026e2225bf22d93fafc5
andrea-genovese <andreagenovese01@gmail.com>
2022-09-03 17:47:58 +0200

    Updated references to CodiceCatastale.java

--- src/main/java/dev/andreagenovese/Main.java
+++ src/main/java/dev/andreagenovese/Main.java
@@ -3,7 +3,7 @@ package dev.andreagenovese;
 public class Main {
     public static void main(String[] args) {
         Person p = new Person("Andrea Giovanni", "Genovese", true, 13, 7, 2001,
-                CodiciCatastali.getCodiceCatastale("Sesto San Giovanni"));
+                CodiceCatastale.getCodiceCatastale("Sesto San Giovanni"));
         System.out.println(p);
         System.out.println(CodiceFiscale.calculate(p));
     }
