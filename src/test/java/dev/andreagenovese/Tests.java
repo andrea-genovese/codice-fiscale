170101045ee374d5a247cb7eff90c2717903b1fb
andrea-genovese <andreagenovese01@gmail.com>
2022-10-30 00:53:16 +0200

    deleted comment

--- src/test/java/dev/andreagenovese/Tests.java
+++ src/test/java/dev/andreagenovese/Tests.java
@@ -2,9 +2,7 @@ package dev.andreagenovese;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-/**
- * Unit test for simple App.
- */
+
 public class Tests {
     Person p = new Person("Andrea Giovanni", "Genovese", true, 13, 7, 2001, "I690");
 
@@ -13,9 +11,7 @@ public class Tests {
         assertEquals('I', CodiceFiscale.controlChar("GNVNRG01L13I690"));
     }
 
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
     public void codiceFiscale() {
         assertEquals("GNVNRG01L13I690I", CodiceFiscale.calculate(p));
